ba8012b9294d7e4bc238f7685ca6dc0303f2ba8f
liuhui <sheldon.ctc@gmail.com>
2012-07-09 22:59:52 +0800

    UPDATE overlap_main.c
    move function overlap to overlap_1, and add new overlap_2 use to compute the
    overlap integral

--- overlap_main.c
+++ overlap_main.c
@@ -1,21 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-//#include <gsl/gsl_linalg.h>
-//#include <gsl/gsl_vector.h>
-//#include <gsl/gsl_blas.h>
-//#include "basis.h"
+#include <gsl/gsl_linalg.h>
+#include <gsl/gsl_vector.h>
+#include <gsl/gsl_blas.h>
+#include "basis.h"
 #include "common.h"
 #include "overlap.h"
 
-int main(int argc, char** argv)
+int overlap_1(char * basis_base)
 {
-    char *basis_base = NULL;
-    if (argc < 2)
-        basis_base = "basis_set";
-    else
-        basis_base = argv[1];
-
     BASIS *b = read_basis(basis_base);    
 
     double result = 0, tmp;
@@ -52,16 +46,16 @@ int main(int argc, char** argv)
 #endif
 
 
-#undef DEBUG_CHECK_INPUT_BASIS
+//#define DEBUG_CHECK_INPUT_BASIS
 #ifdef DEBUG_CHECK_INPUT_BASIS
-    basis_set_output(&b[0], 3, "N-1S:\n");
-    basis_set_output(&b[1], 3, "N-2S:\n");
-    basis_set_output(&b[2], 3, "N-2P:\n");
-    basis_set_output(&b[3], 3, "N-2P:\n");
-    basis_set_output(&b[4], 3, "N-2P:\n");
-    basis_set_output(&b[5], 3, "H-1S:\n");
-    basis_set_output(&b[6], 3, "H-1S:\n");
-    basis_set_output(&b[7], 3, "H-1S:\n");
+    basis_set_output(&b[0], 3, "N-1S:");
+    basis_set_output(&b[1], 3, "N-2S:");
+    basis_set_output(&b[2], 3, "N-2Px:");
+    basis_set_output(&b[3], 3, "N-2Py:");
+    basis_set_output(&b[4], 3, "N-2Pz:");
+    basis_set_output(&b[5], 3, "H-1S:");
+    basis_set_output(&b[6], 3, "H-1S:");
+    basis_set_output(&b[7], 3, "H-1S:");
 #endif
 
     atom_1_basis_num = 5;
@@ -135,3 +129,111 @@ int main(int argc, char** argv)
 
     return 0;
 }
+
+void overlap_2(char* file_name)
+{
+    int i, j, ii,jj;
+    int gauss_i, gauss_j;
+    int basis_count_i, basis_count_j;
+    ATOM_INFO *atom_list;
+    BASIS *basis_i, *basis_j;
+    int atom_count;
+    double result, tmp;
+    gsl_matrix *m_overlap = gsl_matrix_calloc(8,8);
+    int ri = 0, rj = 0;
+
+    INPUT_INFO *b = parse_input(file_name);    
+    //atom_output(b->c, 4);
+
+    atom_list = b->c;
+    atom_count = b->n;
+
+    for (i = 0; i < atom_count; i++) {
+        basis_i = atom_list[i].basis;
+        basis_count_i = atom_list[i].basis_count;
+        for (ii = 0; ii < basis_count_i; ii++) {
+            for (j = 0; j < atom_count; j++) {
+                basis_j = atom_list[j].basis;
+                basis_count_j = atom_list[j].basis_count;
+                for (jj = 0; jj < basis_count_j; jj++) {
+                    result = 0;
+                    for (gauss_i = 0; gauss_i < 3; gauss_i++) {
+                        for (gauss_j = 0; gauss_j < 3; gauss_j++) {
+                            tmp = overlap_single(&basis_i[ii].gaussian[gauss_i], atom_list[i].c, &basis_j[jj].gaussian[gauss_j], atom_list[j].c);
+                            result += tmp;
+                        }
+                    }
+                    //printf("%15.8lf", result);
+                    // 设定一个阀值，如果积分值小于某个数就舍去
+                    if (fabs(result) < 1.0E-10)
+                        result = 0;
+                    gsl_matrix_set(m_overlap, ri, rj, result);
+                    if (rj < 7)
+                        rj++;
+                    else{
+                        rj = 0;
+                        ri++;
+                    }
+                }
+            }
+        }
+    }
+    matrix_output(m_overlap, 8, "Overlap");
+
+}
+
+int main(int argc, char** argv)
+{
+    char *basis_base = NULL;
+    if (argc < 2)
+        basis_base = "basis_set";
+    else
+        basis_base = argv[1];
+    printf("---------Read data from \"%s\"--------------------\n", basis_base);
+    overlap_1(basis_base);
+
+    basis_base = "input_file";
+    printf("---------Read data from \"%s\"--------------------\n", basis_base);
+    overlap_2(basis_base);
+    return 0;
+}
+/*
+$COORD
+N   7   0.30926 0.30926 0.30926
+H   1   2.175   0.0     0.0
+H   1   0.0     2.175   0.0
+H   1   0.0     0.0     2.175
+$END
+$BASIS
+STO-3G
+****
+N   0
+S   3   1.00
+     0.9910616896E+02  0.1543289673E+00
+     0.1805231239E+02  0.5353281423E+00
+     0.4885660238E+01  0.4446345422E+00
+SP   3   1.00
+     0.3780455879E+01 -0.9996722919E-01  0.1559162750E+00
+     0.8784966449E+00  0.3995128261E+00  0.6076837186E+00
+     0.2857143744E+00  0.7001154689E+00  0.3919573931E+00
+****
+H    0
+S   3   1.00
+     0.3425250914E+01  0.1543289673E+00
+     0.6239137298E+00  0.5353281423E+00
+     0.1688554040E+00  0.4446345422E+00
+****
+H   0
+S   3   1.00
+     0.3425250914E+01  0.1543289673E+00
+     0.6239137298E+00  0.5353281423E+00
+     0.1688554040E+00  0.4446345422E+00
+****
+H   0
+S   3   1.00
+     0.3425250914E+01  0.1543289673E+00
+     0.6239137298E+00  0.5353281423E+00
+     0.1688554040E+00  0.4446345422E+00
+****
+$END
+*/
