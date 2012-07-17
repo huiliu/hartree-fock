de34c3e4f22dbba63dfd289ebb52c168a3125f7c
liuhui <sheldon.ctc@gmail.com>
2012-07-17 15:42:57 +0800

    UPDATE debug test condination

--- nuclear_elect.c
+++ nuclear_elect.c
@@ -8,7 +8,7 @@
 
 double check_nuclear(const BASIS* b1, const BASIS* b2, ATOM_INFO **atomList, int atomCount);
 
-double* A_iru(int l1, int l2, double Ax, double Bx, double Cx, double gamma)
+double* A_iru(int l1, int l2, double Ax, double Bx, double Cx, double gamma, int debug)
 {
 // formula (2.18)
     int ii = l1 + l2;
@@ -19,15 +19,15 @@ double* A_iru(int l1, int l2, double Ax, double Bx, double Cx, double gamma)
     A = calloc(sizeof(double), ii);
 
     for (i = 0; i <= ii; i++) {
-        int rr = i / 2;
-        for (r = 0; r <= rr; r++) {
-            int uu = (i - 2*r) / 2;
-            for (u = 0; u <= uu; u++) {
+        for (r = 0; r <= i/2; r++) {
+            for (u = 0; u <= (i-2*r)/2; u++) {
                 I = i - 2*r -u;
                 tmp = pow(-1, u) * factorial(i) * pow(Cx, i - 2*r - 2*u) * \
                         pow(0.25/gamma, r+u) / factorial(r) / factorial(u) / \
                         factorial(i - 2*r - 2*u);
                 A[I] += pow(-1, i) * fi_l_ll_pax_pbx(i, l1, l2, Ax, Bx, flags) * tmp;
+                if (debug == 2)
+                    printf("A_iru = %lf\n", A[I]);
             }
         }
     }
@@ -47,7 +47,7 @@ double F_inc_gamma(int m ,double w)
         for (i = 1; i < F_INC_GAMMA_CYCLE; i++) {
             tmp *= ((2*w) / (2*m + 2*i + 1));
             if ((tmp - F_INC_GAMMA_delta) < 0)
-                return result * factorial_2(2 * m -1) * exp(-w);;
+                break;
             result += tmp;
         }
         return result * factorial_2(2 * m -1) * exp(-w);;
@@ -57,26 +57,23 @@ double F_inc_gamma(int m ,double w)
 }
 
 double nuclear_elect_attraction_gto(const GTO* g1, const gsl_vector* A, \
-        const GTO* g2, const gsl_vector* B, const gsl_vector* C)
+        const GTO* g2, const gsl_vector* B, const gsl_vector* C, int debug)
 {
     int flags = 0;
     gsl_vector* P = gaussian_product_center(g1->alpha, A, g2->alpha, B, flags);
     gsl_vector *PA, *PB, *PC;
     double gamma;
-    double **Axyz;
-    double norm_pc_2, K, normlize_coeff_a, normlize_coeff_b;
+    double *Ax, *Ay, *Az;
+    double norm_pc_2, K;
     double sum = 0, result = 0;
     int i, j, k;
     int l1, n1, m1, l2, m2, n2;
 
-    Axyz = (double **)malloc(sizeof(double *)*3);
     l1 = g1->l; m1 = g1->m; n1 = g1->n;
     l2 = g2->l; m2 = g2->m; n2 = g2->n;
 
     gamma = g1->alpha + g2->alpha;
     K = gauss_K(g1->alpha, A, g2->alpha, B);
-    normlize_coeff_a = normalize_coeff(g1);
-    normlize_coeff_b = normalize_coeff(g2);
 
     PA = gsl_vector_alloc(3);
     PB = gsl_vector_alloc(3);
@@ -86,29 +83,31 @@ double nuclear_elect_attraction_gto(const GTO* g1, const gsl_vector* A, \
     gsl_vector_memcpy(PB, P);
     gsl_vector_memcpy(PC, P);
 
-    norm_pc_2 = pow(gsl_blas_dnrm2(PC), 2);
-    //norm_pc_2 = pow(PC->data[0], 2) + pow(PC->data[1], 2) + pow(PC->data[2], 2);
-
     gsl_vector_sub(PA, A);
     gsl_vector_sub(PB, A);
     gsl_vector_sub(PC, A);
 
+    norm_pc_2 = pow(gsl_blas_dnrm2(PC), 2);
 
-    for (i = 0; i < 3; i++)
-        Axyz[i] = A_iru(g1->l, g2->l, PA->data[i], PB->data[i], PC->data[i], gamma);
+    Ax = A_iru(l1, l2, PA->data[0], PB->data[0], PC->data[0], gamma, debug);
+    Ay = A_iru(m1, m2, PA->data[1], PB->data[1], PC->data[1], gamma, debug);
+    Az = A_iru(n1, n2, PA->data[2], PB->data[2], PC->data[2], gamma, debug);
 
-/*
-\* 等同于上部分
-    Ax = A_iru(g1->l, g2->l, P->data[0] - A->data[0], P->data[0] - B->data[0], P->data[0] - C->data[0], gamma);
-    Ax = A_iru(g1->l, g2->l, P->data[1] - A->data[1], P->data[1] - B->data[1], P->data[1] - C->data[1], gamma);
-    Ax = A_iru(g1->l, g2->l, P->data[2] - A->data[2], P->data[2] - B->data[2], P->data[2] - C->data[2], gamma);
-*/
     for (i = 0; i <= l1 + l2; i++)
         for (j = 0; j <= m1 + m2; j++)
             for (k = 0; k <= n1 + n2; k++)
                 // 公式与 (2.17)不一样
-                sum += Axyz[0][i] * Axyz[1][j] * Axyz[2][k] * F_inc_gamma(i+j+k, norm_pc_2*gamma);
-    result = 2*M_PI / gamma * K * sum * normlize_coeff_a * normlize_coeff_b;
+                sum += Ax[i] * Ay[j] * Az[k] * F_inc_gamma(i+j+k, 
+                                                    norm_pc_2*gamma);
+    result = 2*M_PI / gamma * K * sum * \
+                                g1->norm * g2->norm * g1->coeff * g2->coeff;
+
+if (debug == 2) {
+    vector_output(PA, 3, "PA");
+    vector_output(PB, 3, "PB");
+    vector_output(PC, 3, "PC");
+    printf("norm_PC_2 = %20.10lf sum = %20.10lf result = %20.10lf\n", norm_pc_2, sum, result);
+}
 
     gsl_vector_free(PA);
     gsl_vector_free(PB);
@@ -118,33 +117,49 @@ double nuclear_elect_attraction_gto(const GTO* g1, const gsl_vector* A, \
 }
 
 // compute the nuclear-electrics attraction integrals of one basis
-double nuclear_elect_attraction_basis(const BASIS* b1, const BASIS* b2, ATOM_INFO **atomList, int atomCount)
+double nuclear_elect_attraction_basis(const BASIS* b1, const BASIS* b2, 
+                                ATOM_INFO **atomList, int atomCount, int debug)
 {
     int i, j ,s;
     int gaussCount_1, gaussCount_2;
+    ATOM_INFO *atom;
     double result = 0;
 
     gaussCount_1 = b1->gaussCount;
     gaussCount_2 = b2->gaussCount;
 
+if (debug == 2) {
+    printf("-----------------------\n");
+    basis_set_output(b1, 1, "b1:");
+    basis_set_output(b2, 1, "b2:");
+}
     for (i = 0; i < gaussCount_1; i++) {
         for (j = 0; j < gaussCount_2; j++) {
             for (s = 0; s < atomCount; s++) {
-                result += nuclear_elect_attraction_gto(&b1->gaussian[i], b1->xyz, &b2->gaussian[j], b2->xyz, atomList[s]->coordination);
+                atom = atomList[s];
+                result += nuclear_elect_attraction_gto(
+                    &b1->gaussian[i], b1->xyz, 
+                    &b2->gaussian[j], b2->xyz, 
+                    atom->coordination, debug) * atom->n;
             }
+if (debug == 2) {
+    printf("===============gauss i = %d j = %d================\n", i, j);
+    gto_output(&b1->gaussian[i], 1, "i basis:");
+    gto_output(&b2->gaussian[j], 1, "j basis:");
+}
         }
     }
+if (debug == 2)
+    printf("\n");
 
     return result;
 }
 void nuclear_attraction_matrix(char* file_name)
 {
+    int debug = 0;
     int i, j, basis_count, atomCount;
     BASIS *basisSet;
-    double result, result_check = 0;
-
-    gsl_matrix *m_overlap = gsl_matrix_calloc(8,8);
-    gsl_matrix *m_overlap_c = gsl_matrix_calloc(8,8);
+    double result = 0, result_check = 0;
 
     INPUT_INFO *b = parse_input(file_name);    
 
@@ -153,13 +168,19 @@ void nuclear_attraction_matrix(char* file_name)
     basisSet = b->basisSet;
     atomCount = b->atomCount;
 
+    gsl_matrix *m_overlap = gsl_matrix_calloc(basis_count, basis_count);
+    gsl_matrix *m_overlap_c = gsl_matrix_calloc(basis_count, basis_count);
+
     //atom_output((const ATOM_INFO **)alist, b->atomCount);
     //basis_set_output(b->basisSet, b->basisCount, "BASIS");
 
     for (i = 0; i <  basis_count; i++) {
         for (j = 0; j < basis_count; j++) {
+            debug = 0;
+            if ( (i == j) && j == 3)
+                debug = 2;
             result = nuclear_elect_attraction_basis(&basisSet[i], &basisSet[j],
-                            alist, atomCount);
+                            alist, atomCount, debug);
             result_check = check_nuclear(&basisSet[i], &basisSet[j],
                             alist, atomCount);
             // 设定一个阀值，如果积分值小于某个数就舍去
@@ -171,8 +192,8 @@ void nuclear_attraction_matrix(char* file_name)
             gsl_matrix_set(m_overlap_c, i, j, result_check);
         }
     }
-    matrix_output(m_overlap, 8, "NUCLEAR");
-    matrix_output(m_overlap_c, 8, "CHECH NUCLEAR");
+    matrix_output(m_overlap, basis_count, "NUCLEAR");
+    matrix_output(m_overlap_c, basis_count, "CHECH NUCLEAR");
 }
 
 double check_nuclear(const BASIS* b1, const BASIS* b2, ATOM_INFO **atomList, int atomCount)
@@ -208,7 +229,7 @@ double check_nuclear(const BASIS* b1, const BASIS* b2, ATOM_INFO **atomList, int
             zb = gsl_vector_get(B, 2);
 
             for (s = 0; s < atomCount; s++) {
-                C = atomList[i]->coordination;
+                C = atomList[s]->coordination;
 
                 xc = gsl_vector_get(C, 0);
                 yc = gsl_vector_get(C, 1);
