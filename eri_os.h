0d49f7db0b866f43b09668c86c079dc8f6a8430f
liuhui <sheldon.ctc@gmail.com>
2012-08-26 21:20:12 +0800

    REMOVE THE HRR CODE
    now, the code only contain the OS scheme.

--- eri_os.h
+++ eri_os.h
@@ -2,7 +2,12 @@
 #include <gsl/gsl_blas.h>
 #include <gsl/gsl_math.h>
 #include <gsl/gsl_sf_exp.h>
-double K_OS(double a, double b, double AB_2);
+
+
+#ifndef INTEGRALS__ERI__OS_SCHEME
+#define INTEGRALS__ERI__OS_SCHEME
+
+#define K_OS(a, b, AB_2) M_SQRT2 * pow(M_PI, 1.25) / (a + b) * exp(-a*b/(a+b) * AB_2)
 double ERI_basis_OS(const BASIS* b1, const BASIS* b2,
                     const BASIS* b3, const BASIS* b4, int debug);
 double ERI_VRR_OS(int l1, int m1, int n1,
@@ -13,12 +18,5 @@ double ERI_VRR_OS(int l1, int m1, int n1,
                   const gsl_vector *PA, const gsl_vector *PB, const gsl_vector *QC,
                   const gsl_vector *QD, const gsl_vector *WQ, const gsl_vector *WP,
                   int m, double*);
-double ERI_HRR_OS(int l1, int m1, int n1,
-                  int l2, int m2, int n2,
-                  int l3, int m3, int n3,
-                  int l4, int m4, int n4,
-                  double zeta, double gamma, double ro,
-                  const gsl_vector *AB, const gsl_vector *CD,
-                  const gsl_vector *PA, const gsl_vector *PB, const gsl_vector *QC,
-                  const gsl_vector *QD, const gsl_vector *WQ, const gsl_vector *WP,
-                  double T);
+
+#endif
